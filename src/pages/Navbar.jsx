cf3f7019edcc319e785056b6830e1bed2e87407d
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-30 19:10:38 +0530

    nav

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -6,10 +6,10 @@ function Navbar() {
     <div className='nav'>
       
          <ul className='menu'>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/About">About</Link></li>
-            <li><Link to="/Contact">Contact</Link></li>
-            <li><Link to="/Projects">Projects</Link></li>
+            <li className='nav-links'><Link to="/">Home</Link></li>
+            <li className='nav-links'><Link to="/About">About</Link></li>
+            <li className='nav-links'><Link to="/Contact">Contact</Link></li>
+            <li className='nav-links'><Link to="/Projects">Projects</Link></li>
 
          </ul>
 
