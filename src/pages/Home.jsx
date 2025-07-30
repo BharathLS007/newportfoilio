424ab5af403d295ab2d6b9cf001dbd1707122841
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-30 19:58:27 +0530

    home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,9 @@ function Home() {
         <Navbar/>
       <div className="container">
         <div className="content-right">
-          <h1>Hi There!</h1>
+          <h1>Hi There!</h1><br />
+          <p className='title'>I'm <span className='highlight'>Bharath L S</span></p><br />
+          <p className='title'><span className='highlight'>Frontend Developer</span></p>
         </div>
  
         <div className="content-left">
