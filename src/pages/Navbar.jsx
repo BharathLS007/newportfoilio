0143dfd961450e77b58cb0fbcb87bd089644075d
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-30 19:28:11 +0530

    nav

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react';
 
 function Navbar() {
+  useEffect(() => {
+    const nav = document.querySelector('.nav');
+
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+      } else {
+        nav.classList.remove('scrolled');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className='nav'>
-      
-         <ul className='menu'>
-            <li className='nav-links'><Link to="/">Home</Link></li>
-            <li className='nav-links'><Link to="/About">About</Link></li>
-            <li className='nav-links'><Link to="/Contact">Contact</Link></li>
-            <li className='nav-links'><Link to="/Projects">Projects</Link></li>
-
-         </ul>
-
+      <ul className='menu'>
+        <li className='nav-links'><Link to="/">Home</Link></li>
+        <li className='nav-links'><Link to="/About">About</Link></li>
+        <li className='nav-links'><Link to="/Contact">Contact</Link></li>
+        <li className='nav-links'><Link to="/Projects">Projects</Link></li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Navbar
