72b8297d33ce777f1a906c8cdf1191b892915a94
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-30 20:09:29 +0530

    type write fuction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import work from '../assets/work.svg'
+import { Typewriter } from 'react-simple-typewriter';
 function Home() {
   return (
     <div>
@@ -8,8 +9,21 @@ function Home() {
       <div className="container">
         <div className="content-right">
           <h1>Hi There!</h1><br />
-          <p className='title'>I'm <span className='highlight'>Bharath L S</span></p><br />
-          <p className='title'><span className='highlight'>Frontend Developer</span></p>
+          <p className='title'>I'm <span className='highlight'>Bharath L S</span><span class="wave">👋</span></p><br />
+          <p className='title'>
+  <span className='highlight'>
+    <Typewriter
+      words={['Frontend Developer', 'Software Developer']}
+      loop={true}
+      cursor
+      cursorStyle='|'
+      typeSpeed={70}
+      deleteSpeed={50}
+      delaySpeed={1000}
+    />
+  </span>
+</p>
+
         </div>
  
         <div className="content-left">
