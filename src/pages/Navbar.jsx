1aaf61e9ed2b2aee9f31d696c7c9d41d9bddde83
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-31 19:20:14 +0530

    home and nav

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   useEffect(() => {
     const nav = document.querySelector('.nav');
 
@@ -19,16 +20,30 @@ function Navbar() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className='nav'>
-      <ul className='menu'>
-        <li className='nav-links'><Link to="/">Home</Link></li>
-        <li className='nav-links'><Link to="/About">About</Link></li>
-        <li className='nav-links'><Link to="/Contact">Contact</Link></li>
-        <li className='nav-links'><Link to="/Projects">Projects</Link></li>
-      </ul>
-    </div>
+    <nav className="nav">
+      <div className="nav-container">
+        <div className="logo">
+          
+        </div>
+        <div className={`menu-icon ${menuOpen ? 'active' : ''}`} onClick={toggleMenu} aria-label="Toggle menu" role="button" tabIndex={0} onKeyPress={toggleMenu}>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </div>
+        <ul className={`menu ${menuOpen ? 'active' : ''}`}>
+          <li className="nav-links"><Link to="/">Home</Link></li>
+          <li className="nav-links"><Link to="/About">About</Link></li>
+          <li className="nav-links"><Link to="/Contact">Contact</Link></li>
+          <li className="nav-links"><Link to="/Projects">Projects</Link></li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
-export default Navbar
+export default Navbar;
