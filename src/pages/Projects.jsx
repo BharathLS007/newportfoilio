db73134b18d86988f814c49c02c5fbc3b83cfcbf
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-01 19:20:02 +0530

    push

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,13 @@ function Projects() {
   return (
     <div>
       <Navbar/>
-        <h1>THis is Project</h1>
+        <div className="container">
+          <div className='works'>
+            <h1 className='intro'>My Recent <span className='highlight'>Works</span></h1>
+            <p style={{ fontSize: '1.3rem' }}>Here are a few project i've worked on recently </p>
+          </div>
+
+        </div>
     </div>
   )
 }
