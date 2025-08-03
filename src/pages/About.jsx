180605d01e850a6648fe7ab63481cfb45ca2d973
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-03 14:04:40 +0530

    c

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,17 +19,31 @@ function About() {
     <div>
             <Navbar/>
            <div className="container">
-            <div className="content-right">
-              <h1>Know Who <span className='highlight'>I'M</span></h1><br />
-              <p style={{ fontSize: '1.3rem' }}>Hi Everyone,<span className='highlight'> I am Bharath L S </span>from <span className='highlight'>Kanyakumari</span></p>
-              <p style={{ fontSize: '1.3rem' }}>I am recently completed my Bacholors in Computer Science  </p>
-              <p style={{ fontSize: '1.3rem' }}>and Engineering from University College of Engineering,Nagercoil</p><br />
-              <p style={{ fontSize: '1.3rem'}}>Apart from Coding,Some Otheractivities that I love to do!</p><br />
-              <p style={{ fontSize:'1.3rem',marginLeft:'20px'}}><i className="fa-solid fa-hand-point-right" ></i>Gaming</p><br />
-              <p style={{ fontSize:'1.3rem',marginLeft:'20px'}}><i className="fa-solid fa-hand-point-right" ></i>Reading Books</p><br />
-              <p style={{ fontSize:'1.3rem',marginLeft:'20px'}}><i className="fa-solid fa-hand-point-right" ></i>Travelling</p><br />
+<div className="content-right">
+  <h1>Know Who <span className='highlight'>I Am</span></h1><br />
+  <p style={{ fontSize: '1.3rem' }}>
+    Hi Everyone, <span className='highlight'>I am Bharath L S</span> from <span className='highlight'>Kanyakumari</span>
+  </p>
+  <p style={{ fontSize: '1.3rem' }}>
+    I recently completed my Bachelor's in Computer Science
+  </p>
+  <p style={{ fontSize: '1.3rem' }}>
+    and Engineering from University College of Engineering, Nagercoil
+  </p><br />
+  <p style={{ fontSize: '1.3rem' }}>
+    Apart from coding, some other activities that I love to do:
+  </p><br />
+  <p style={{ fontSize:'1.3rem', marginLeft:'20px' }}>
+    <i className="fa-solid fa-hand-point-right"></i> Gaming
+  </p><br />
+  <p style={{ fontSize:'1.3rem', marginLeft:'20px' }}>
+    <i className="fa-solid fa-hand-point-right"></i> Reading Books
+  </p><br />
+  <p style={{ fontSize:'1.3rem', marginLeft:'20px' }}>
+    <i className="fa-solid fa-hand-point-right"></i> Travelling
+  </p><br />
+</div>
 
-            </div>
             <div className="content-left">
               <img src={job} alt="img" />
             </div>
